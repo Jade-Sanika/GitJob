99bcc80d070975dc2c0bc222d5e92c875783224d
Jade-Sanika <149955263+Jade-Sanika@users.noreply.github.com>
2025-02-13 22:28:28 +0530

    Update Javacode.java

--- Javacode.java
+++ Javacode.java
@@ -2,6 +2,7 @@ public class Javacode {
     public static void main(String[] args) {
         System.out.println("Hello, World!");
         System.out.println("Sanika Jade");
+        System.out.println("TE INFT B");
     }
 }
 
